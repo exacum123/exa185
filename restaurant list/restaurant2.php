02ad4fe396ec0d63c9d8ffc8bfcf72c5d07a5919
exacum123 <79459051+exacum123@users.noreply.github.com>
2021-03-06 01:56:42 +0800

    Add files via upload

---
+++ restaurant list/restaurant2.php
@@ -0,0 +1,53 @@
+<?php
+include('../Header.php');
+$jsondata = json_decode(file_get_contents("../data.json"), true);
+?>
+
+<title>
+  <?php echo $jsondata['restaurant'][2]["name"] ?>
+</title>
+
+
+<section class="container">
+  <?php
+  echo "<br>" . "<h2>" . $jsondata['restaurant'][2]["name"] . "</h2>" . "<br>";
+  echo "<h6>" . "Intro: " . $jsondata['restaurant'][2]["intro"] . "</h6>" . "<br>" . "<br>" . "<br>";
+  ?>
+</section>
+
+<div class="container justify-content-start">
+  <div class="row">
+    <div class="col-3">
+    <img src="../img/Logo/Running 燒Logo.jfif" width="300px" height="300px">
+      <?php
+      echo "Tel: " . $jsondata['restaurant'][2]["tel"] . "<br>";
+      echo "Address: " . $jsondata['restaurant'][2]["adrs"] . "<br>" . "<br>" . "<br>" . "<br>";
+      echo "Opening Hours: " . "<br>" . "<br>";
+      echo "<h5>" . "Mon-Fri:" . $jsondata['restaurant'][2]['time'][0]['MonFri'] . "</h5>";
+      echo "<h5>" . "Sat-Sun:" . $jsondata['restaurant'][2]['time'][0]['SatSun'] . "</h5>" . "<br>" . "<br>";
+      ?>
+    </div>
+    <div class="col-7 offset-2">
+      <?php
+      echo "<h4>" . "The MOST Popular dishes:" . "</h4>" . "<br>" . "<br>". "<br>";
+
+      echo '<img src="../img/dish1.jfif">'."<br>";
+      echo "No.1" . "<p>" . $jsondata['restaurant'][2]["food"][0]['id0'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish2.jfif">'."<br>";
+      echo "No.2" . "<p>" . $jsondata['restaurant'][2]["food"][0]['id1'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish3.jfif">'."<br>";
+      echo "No.3" . "<p>" . $jsondata['restaurant'][2]["food"][0]['id2'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish4.jfif">'."<br>";
+      echo "No.4" . "<p>" . $jsondata['restaurant'][2]["food"][0]['id3'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish5.jfif">'."<br>";
+      echo "No.5" . "<p>" . $jsondata['restaurant'][2]["food"][0]['id4'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish6.jfif">'."<br>";
+      echo "No.6" . "<p>" . $jsondata['restaurant'][2]["food"][0]['id5'] . "</p>" . "<br>" . "<br>" . "<br>";
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php
+include('../footer.php');
+?>
---
+++ restaurant list/restaurant5.php
@@ -0,0 +1,53 @@
+<?php
+include('../Header.php');
+$jsondata = json_decode(file_get_contents("../data.json"), true);
+?>
+
+<title>
+  <?php echo $jsondata['restaurant'][5]["name"] ?>
+</title>
+
+
+<section class="container">
+  <?php
+  echo "<br>" . "<h2>" . $jsondata['restaurant'][5]["name"] . "</h2>" . "<br>";
+  echo "<h6>" . "Intro: " . $jsondata['restaurant'][5]["intro"] . "</h6>" . "<br>" . "<br>" . "<br>";
+  ?>
+</section>
+
+<div class="container justify-content-start">
+  <div class="row">
+    <div class="col-3">
+    <img src="../img/Logo/蘇杭冰室Logo.jfif" width="300px" height="300px">
+      <?php
+      echo "Tel: " . $jsondata['restaurant'][5]["tel"] . "<br>";
+      echo "Address: " . $jsondata['restaurant'][5]["adrs"] . "<br>" . "<br>" . "<br>" . "<br>";
+      echo "Opening Hours: " . "<br>" . "<br>";
+      echo "<h5>" . "Mon-Fri:" . $jsondata['restaurant'][5]['time'][0]['MonFri'] . "</h5>";
+      echo "<h5>" . "Sat-Sun:" . $jsondata['restaurant'][5]['time'][0]['SatSun'] . "</h5>" . "<br>" . "<br>";
+      ?>
+    </div>
+    <div class="col-7 offset-2">
+      <?php
+      echo "<h4>" . "The MOST Popular dishes:" . "</h4>" . "<br>" . "<br>". "<br>";
+
+      echo '<img src="../img/dish1.jfif">'."<br>";
+      echo "No.1" . "<p>" . $jsondata['restaurant'][5]["food"][0]['id0'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish2.jfif">'."<br>";
+      echo "No.2" . "<p>" . $jsondata['restaurant'][5]["food"][0]['id1'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish3.jfif">'."<br>";
+      echo "No.3" . "<p>" . $jsondata['restaurant'][5]["food"][0]['id2'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish4.jfif">'."<br>";
+      echo "No.4" . "<p>" . $jsondata['restaurant'][5]["food"][0]['id3'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish5.jfif">'."<br>";
+      echo "No.5" . "<p>" . $jsondata['restaurant'][5]["food"][0]['id4'] . "</p>" . "<br>" . "<br>" . "<br>";
+      echo '<img src="../img/dish6.jfif">'."<br>";
+      echo "No.6" . "<p>" . $jsondata['restaurant'][5]["food"][0]['id5'] . "</p>" . "<br>" . "<br>" . "<br>";
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php
+include('../footer.php');
+?>
