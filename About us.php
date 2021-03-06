9b46e102934befa545e816ef8462c75eb11ae5de
exacum123 <79459051+exacum123@users.noreply.github.com>
2021-03-06 11:36:41 +0800

    Add files via upload

---
+++ About us.php
@@ -0,0 +1,16 @@
+<?php
+include('Header.php');
+?>
+
+<body style="background-color: rgb(249, 255, 255);">
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">About Us</h1>
+    <p class="lead">This is a project to display what I have learned from programming languages. </p>
+  </div>
+</div>
+</body>
+
+<?php 
+include('footer.php');
+?>
