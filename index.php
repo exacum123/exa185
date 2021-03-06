a1992200fdb39d512c936684d6e1d9affd26fefa
exacum123 <79459051+exacum123@users.noreply.github.com>
2021-03-06 11:18:34 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -8,38 +8,48 @@ $GLOBALS['jsondata'] = json_decode(file_get_contents("data.json"), true);
     Welcome to little restaurants intro.
 </title>
 
-<div class="container">
-    <div class="row">
+<body style="background-color: rgb(249, 255, 255);">
+    <div class="container">
+        <div class="row">
 
-        <h2>Restaurants list:</h2><br><br><br>
+            <h2>Restaurants list:</h2><br><br><br>
 
-        <section class="container mb-2">
-            <?php
+            <section class="container mb-2">
+                <?php
 
-            $jsondata = json_decode(file_get_contents("data.json"));
+                $jsondata = json_decode(file_get_contents("data.json"));
 
-            foreach ($jsondata->restaurant as $json1) {
-            ?>
-            <div id=imgForRest>
-            <a target="_blank" href="<?php echo "$json1->url"?>">
-            <img src="<?php echo $json1->imgLogo ?>"></a>
-            </div>
-            <?php
-                echo "<h5>"."<strong>"."$json1->name"."</strong>"."</h5>";
-                echo "Tel: " . $json1->tel . "<br>";
-                echo "Address: " . $json1->adrs . "<br>" . "<br>" . "<br>";
-                echo "Opening Hours: " . "<br>" . "<br>";
-                foreach ($json1->time as $json2) {
-                    echo "Mon-Fri:" . $json2->MonFri . "<br>";
-                    echo "Sat-Sun:" . $json2->SatSun . "<br>" . "<br>";
+                foreach ($jsondata->restaurant as $json1) {
+                ?>
+                    <div class="mb-3" id=imgForRest >
+                        <a target="_blank" href="<?php echo "$json1->url" ?>">
+                            <img src="<?php echo $json1->imgLogo ?>"></a>
+                    </div>
+                    <?php echo "<h5>" . "<strong>" . "$json1->name" . "</strong>" . "</h5>"; ?>
+                    <div class="card mt-3 mb-4" style="width: 19rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" id="barColor"><?php echo "Tel: " . $json1->tel; ?></li>
+                        </ul>
+                    </div>
+                    <?php echo "Opening Hours: " . "<br>" . "<br>"; ?>
+                    <div id="barColor" class="card mb-5" style="width: 19rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" id="barColor"><?php foreach ($json1->time as $json2) {
+                        echo "Mon-Fri:" . $json2->MonFri . "<br>";} ?></li>
+                            <li class="list-group-item" id="barColor"><?php foreach ($json1->time as $json2) {
+                                echo "Sat-Sun:" . $json2->SatSun;} ?></li>
+                        </ul>
+                    </div>
+                
+                <?php
                 }
-            }
-            ?>
+                ?>
 
-        </section>
+            </section>
 
+        </div>
     </div>
-</div>
+</body>
 <?php
 include('footer.php')
 ?>
